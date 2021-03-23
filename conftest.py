eeb3dd29555b0264c266e435a86ae953eef1f36a
Tony <380996055598af@gmail.com>
2021-03-23 17:25:18 +0200

    Committed latest changes as of passing lesson 4_2_step6. https://stepik.org/lesson/238819/step/4?unit=211271 Including latest changes - implicitly wait try\catch of NoSuchElementException handling.

--- conftest.py
+++ conftest.py
@@ -15,12 +15,12 @@ def browser(request):
     user_language = request.config.getoption("language")
     languages_list = ['es', 'fr', 'en', 'ru']
     if user_language in languages_list:
-        print("\nStart browser for testing languages selection...")
+        print("\n...Start browser...")
         options = Options()
         options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
         browser = webdriver.Chrome(options=options)
     else:
         raise pytest.UsageError(f"--language should be set and should be in {languages_list}")
     yield browser
-    print("\nClosing browser...")
+    print("\n...Closing browser...")
     browser.quit()
