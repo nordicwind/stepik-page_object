f9b4f7ee5c289cb39f770446949f2fcdc0b62e7e
Tony <380996055598af@gmail.com>
2021-03-24 00:03:25 +0200

    Added login_page.py and added tests into test_main_page.py.

--- test_main_page.py
+++ test_main_page.py
@@ -1,15 +1,25 @@
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
+
+
+link = "http://selenium1py.pythonanywhere.com"
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
 
 
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+
+def test_login_page_verification(browser):
+    login_link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, login_link)
+    page.open()
+    page.should_be_login_page()
