4b179b6c3d73be59aa287de9cda3c91113406842
Tony <380996055598af@gmail.com>
2021-03-24 16:42:40 +0200

    Modified test_guest_can_go_to_login_page method in test_main_page.py file to contain latest changes.

--- test_main_page.py
+++ test_main_page.py
@@ -6,20 +6,16 @@ link = "http://selenium1py.pythonanywhere.com"
 
 
 def test_guest_can_go_to_login_page(browser):
+    # link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
+    # link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
-
-
-
-def test_login_page_verification(browser):
-    login_link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page = LoginPage(browser, login_link)
-    page.open()
-    page.should_be_login_page()
