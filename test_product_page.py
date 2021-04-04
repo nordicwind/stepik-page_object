dad12bd8df6a876dd4594e9dc85d0f9e4a0c5a7b
Tony <380996055598af@gmail.com>
2021-04-04 22:44:08 +0300

    Made latest changes accordingly to requirements for lesson 4_3_step13 https://stepik.org/lesson/201964/step/13?unit=176022

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,8 @@
 import pytest
 from pages.product_page import ProductPage
+from pages.basket_page import BasketPage
+from pages.login_page import LoginPage
+import time
 
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
@@ -17,6 +20,35 @@ parameters = [
 ]
 
 
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        email = str(time.time()) + "@mailinator.com"
+        password = str(time.time()) + "@mailinator.com"
+        page = ProductPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.register_new_user(email, password)
+        page.should_be_authorized_user()
+
+    # Removed parameter as required
+    def test_user_cant_see_success_message(self, browser):
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
+
+    # @pytest.mark.skip
+    def test_user_can_add_product_to_basket(self, browser):
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_product_to_basket()
+        page.solve_quiz_and_get_code()
+        page.success_message_should_appear()
+        page.product_title_should_match_product_in_basket()
+        page.product_price_and_basket_should_match()
+
+
 @pytest.mark.parametrize('parameter', parameters)
 @pytest.mark.skip
 def test_guest_can_add_product_to_basket(browser, parameter):
@@ -29,18 +61,18 @@ def test_guest_can_add_product_to_basket(browser, parameter):
     page.product_price_and_basket_should_match()
 
 
+def test_guest_cant_see_success_message(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+
 @pytest.mark.skip
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
     page.solve_quiz_and_get_code()
-    page.should_not_be_success_message()
-
-
-def test_guest_cant_see_success_message(browser):
-    page = ProductPage(browser, link)
-    page.open()
     page.should_not_be_success_message()
 
 
@@ -52,15 +84,21 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
 
 
 def test_guest_should_see_login_link_on_product_page(browser):
-    _link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, _link)
+    page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
 
 
 def test_guest_can_go_to_login_page_from_product_page(browser):
-    _link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, _link)
+    page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
     page.should_be_login_link()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    page = BasketPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    page.should_be_empty_basket()
+    page.should_be_empty_basket_text()
