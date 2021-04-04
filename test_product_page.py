e0d88fbf49bbd276209af5f73e2c83ed65b7a2bc
Tony <380996055598af@gmail.com>
2021-04-04 23:14:38 +0300

    Made latest changes accordingly to requirements for lesson 4_3_step13 https://stepik.org/lesson/201964/step/13?unit=176022

--- test_product_page.py
+++ test_product_page.py
@@ -5,19 +5,20 @@ from pages.login_page import LoginPage
 import time
 
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-parameters = [
-    '?promo=offer0',
-    '?promo=offer1',
-    '?promo=offer2',
-    '?promo=offer3',
-    '?promo=offer4',
-    '?promo=offer5',
-    '?promo=offer6',
-    pytest.param('?promo=offer7', marks=pytest.mark.xfail),
-    '?promo=offer8',
-    '?promo=offer9',
-]
+link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/"
+# previous version with parameters
+# parameters = [
+#     '?promo=offer0',
+#     '?promo=offer1',
+#     '?promo=offer2',
+#     '?promo=offer3',
+#     '?promo=offer4',
+#     '?promo=offer5',
+#     '?promo=offer6',
+#     pytest.param('?promo=offer7', marks=pytest.mark.xfail),
+#     '?promo=offer8',
+#     '?promo=offer9',
+# ]
 
 
 class TestUserAddToBasketFromProductPage():
@@ -39,23 +40,22 @@ class TestUserAddToBasketFromProductPage():
         page.should_not_be_success_message()
 
     # @pytest.mark.skip
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         page = ProductPage(browser, link)
         page.open()
         page.add_product_to_basket()
-        page.solve_quiz_and_get_code()
         page.success_message_should_appear()
         page.product_title_should_match_product_in_basket()
         page.product_price_and_basket_should_match()
 
 
-@pytest.mark.parametrize('parameter', parameters)
-@pytest.mark.skip
-def test_guest_can_add_product_to_basket(browser, parameter):
-    page = ProductPage(browser, f"{link}{parameter}")
+@pytest.mark.need_review
+# @pytest.mark.skip
+def test_guest_can_add_product_to_basket(browser):
+    page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
-    page.solve_quiz_and_get_code()
     page.success_message_should_appear()
     page.product_title_should_match_product_in_basket()
     page.product_price_and_basket_should_match()
@@ -88,14 +88,14 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
-
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
     page.should_be_login_link()
 
-
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page = BasketPage(browser, link)
     page.open()
